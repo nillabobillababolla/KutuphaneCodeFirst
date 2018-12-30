90c36cd7158bd19911660acb0778c90f5065427a
Samet Alabey <samet.alabey@hotmail.com>
2018-12-30 22:23:18 +0300

    Kitap ödünç verme.

--- KutuphaneCodeFirst/KiralamaIslemleri.Designer.cs
+++ KutuphaneCodeFirst/KiralamaIslemleri.Designer.cs
@@ -28,20 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lstKitaplar = new System.Windows.Forms.ListBox();
+            this.cmbKiralayanlar = new System.Windows.Forms.ComboBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lstKitaplar
+            // 
+            this.lstKitaplar.FormattingEnabled = true;
+            this.lstKitaplar.Location = new System.Drawing.Point(12, 12);
+            this.lstKitaplar.Name = "lstKitaplar";
+            this.lstKitaplar.Size = new System.Drawing.Size(437, 95);
+            this.lstKitaplar.TabIndex = 0;
+            // 
+            // cmbKiralayanlar
+            // 
+            this.cmbKiralayanlar.FormattingEnabled = true;
+            this.cmbKiralayanlar.Location = new System.Drawing.Point(12, 113);
+            this.cmbKiralayanlar.Name = "cmbKiralayanlar";
+            this.cmbKiralayanlar.Size = new System.Drawing.Size(437, 21);
+            this.cmbKiralayanlar.TabIndex = 1;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(374, 140);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnKaydet.TabIndex = 2;
+            this.btnKaydet.Text = "Kitabı Ver";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
             // 
             // KiralamaIslemleri
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.cmbKiralayanlar);
+            this.Controls.Add(this.lstKitaplar);
             this.Name = "KiralamaIslemleri";
             this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
             this.Text = "KiralamaIslemleri";
+            this.Load += new System.EventHandler(this.KiralamaIslemleri_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstKitaplar;
+        private System.Windows.Forms.ComboBox cmbKiralayanlar;
+        private System.Windows.Forms.Button btnKaydet;
     }
 }
--- KutuphaneCodeFirst/KiralamaIslemleri.cs
+++ KutuphaneCodeFirst/KiralamaIslemleri.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using KutuphaneCodeFirst.Entities;
+using KutuphaneCodeFirst.MockData;
 
 namespace KutuphaneCodeFirst
 {
@@ -15,6 +11,65 @@ namespace KutuphaneCodeFirst
         public KiralamaIslemleri()
         {
             InitializeComponent();
+        }
+
+        private void KiralamaIslemleri_Load(object sender, EventArgs e)
+        {
+            ListeDoldur();
+        }
+
+        private void ListeDoldur()
+        {
+            var db = new MyContext();
+            Mock.Yazarlar = db.Yazarlar.ToList();
+            Mock.Kiralayanlar = db.Kiralayanlar.ToList();
+            lstKitaplar.DataSource = Mock.Kitaplar;
+            cmbKiralayanlar.DataSource = Mock.Kiralayanlar;
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            var db = new MyContext();
+            using (var tran = db.Database.BeginTransaction())
+            {
+                try
+                {
+                    if (cmbKiralayanlar.SelectedItem==null) return;
+                    if (lstKitaplar.SelectedItem != null)
+                    {
+                        var kitap = (Kitap)lstKitaplar.SelectedItem;
+                        var seciliKitap = db.Kitaplar.SingleOrDefault(b => b.KitapId == kitap.KitapId);
+
+                        var kiralayan = (Kiralayan)cmbKiralayanlar.SelectedItem;
+
+                        if (seciliKitap.Adet==0)
+                        {
+                            MessageBox.Show(@"Kitap Stokta Yok!");
+                            return;
+                        }
+
+                        var kira = new Kira
+                        {
+                            KiralamaTarihi = DateTime.Now,
+                            KiralayanId = kiralayan.KiralayanId,
+                            KitapId = seciliKitap.KitapId,
+                        };
+
+                        db.Kiralar.Add(kira);
+                        seciliKitap.Adet--;
+                        db.SaveChanges();
+                        tran.Commit();
+                        MessageBox.Show($@"{kitap} kitabı {kiralayan}'a verildi.");
+                        ListeDoldur();
+                    }
+                }
+                catch (Exception exception)
+                {
+                    tran.Rollback();
+                    MessageBox.Show(exception.Message);
+                }
+            }
+
         }
     }
 }
