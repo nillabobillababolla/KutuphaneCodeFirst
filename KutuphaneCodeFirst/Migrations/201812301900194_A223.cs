6ba50cdf6006a14c4e4ca69c5bf3ba0e67cf346a
Samet Alabey <samet.alabey@hotmail.com>
2018-12-30 22:00:51 +0300

    Migration eklendi.

---
+++ KutuphaneCodeFirst/Migrations/201812301900194_A223.cs
@@ -0,0 +1,15 @@
+namespace KutuphaneCodeFirst.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class A223 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
